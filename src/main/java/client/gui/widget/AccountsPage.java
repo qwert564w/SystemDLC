e1a9b88ae746032e210941e0b6f7768696864e7e
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-09-25 16:27:55 +0300

    Fix account manager rendering

--- src/main/java/client/gui/widget/AccountsPage.java
+++ src/main/java/client/gui/widget/AccountsPage.java
@@ -1,6 +1,7 @@
 package client.gui.widget;
 
 import client.module.Feature;
+import client.api.Theme;
 import client.render.TextShader;
 import java.util.ArrayList;
 import java.util.List;
@@ -67,12 +68,12 @@ public final class AccountsPage extends Widget {
     public void onFloatFloatFloatMatrix4f(float value, float value2, float value3, Matrix4f matrix4f) {
         float x = this.value235 + 24.0F;
         float y = this.value236 + 24.0F;
-        int foreground = this.foreground();
-        int muted = this.mutedFg();
+        int foreground = Theme.foreground();
+        int muted = Theme.mutedFg();
 
         TextShader.onFloatStringFloatFloatIntFloatMatrix4f(value, "Account Manager", 20.0F, x, foreground, y, matrix4f);
-        TextShader.onFloatStringFloatFloatIntFloatMatrix4f(value, "Текущий: " + getCurrentName(), 14.0F, x, y + 28.0F, foreground, y + 28.0F, matrix4f);
-        TextShader.onFloatStringFloatFloatIntFloatMatrix4f(value, "Сохранить текущий аккаунт", 14.0F, x, y + 60.0F, foreground, y + 60.0F, matrix4f);
+        TextShader.onFloatStringFloatFloatIntFloatMatrix4f(value, "Текущий: " + getCurrentName(), 14.0F, x, foreground, y + 28.0F, matrix4f);
+        TextShader.onFloatStringFloatFloatIntFloatMatrix4f(value, "Сохранить текущий аккаунт", 14.0F, x, foreground, y + 60.0F, matrix4f);
 
         float rowY = y + 112.0F;
         for (int i = 0; i < profiles.size(); i++) {
